f9be8b6583285b552597ae15aa3f76dbce1f61e2
kennyonearth28-source <kenny.onearth28@gmail.com>
2025-08-19 01:40:52 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.dutchie.com"], // Add the domain of your images
+  },
+};
+
+module.exports = nextConfig;
